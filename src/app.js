f17e193560360080318ce57ce28983a207013684
IBRAHIMTARIQ18 <ibrahimtariq2014@gmail.com>
2025-08-20 00:13:48 +0500

    Add Express server setup with CORS and basic configurations

---
+++ src/app.js
@@ -0,0 +1,25 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+// Basic configurations
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+
+// Enable CORS for all routes
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN?.split(",") || "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Autohorization"],
+  }),
+);
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the project Management Application!");
+});
+
+export default app;
