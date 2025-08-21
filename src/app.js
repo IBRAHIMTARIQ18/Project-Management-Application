75e3d10e503c45721e5fe2c757c9f6207734b41e
IBRAHIMTARIQ18 <ibrahimtariq2014@gmail.com>
2025-08-21 23:49:55 +0500

    Add health check route and controller for server health status

--- src/app.js
+++ src/app.js
@@ -18,6 +18,12 @@ app.use(
   }),
 );
 
+// Import routes
+import healthCheckRouter from "./routes/healthCheck.route.js";
+
+// Use routes
+app.use("/api/v1/healthcheck", healthCheckRouter);
+
 app.get("/", (req, res) => {
   res.send("Welcome to the project Management Application!");
 });
